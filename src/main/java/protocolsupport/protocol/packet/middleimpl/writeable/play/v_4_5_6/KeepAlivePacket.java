a06989fcee6bee4cf4bd584fc07e7b9258227884
Gilberto <73399601+GilbertoKPL@users.noreply.github.com>
2026-04-19 10:15:38 -0300

    Add Forge-focused keepalive and plugin message debug traces

--- src/main/java/protocolsupport/protocol/packet/middleimpl/writeable/play/v_4_5_6/KeepAlivePacket.java
+++ src/main/java/protocolsupport/protocol/packet/middleimpl/writeable/play/v_4_5_6/KeepAlivePacket.java
@@ -1,11 +1,16 @@
 package protocolsupport.protocol.packet.middleimpl.writeable.play.v_4_5_6;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.util.internal.logging.InternalLogger;
+import io.netty.util.internal.logging.InternalLoggerFactory;
 import net.md_5.bungee.protocol.packet.KeepAlive;
 import protocolsupport.protocol.packet.id.LegacyPacketId;
 import protocolsupport.protocol.packet.middleimpl.writeable.LegacySingleWriteablePacket;
 
 public class KeepAlivePacket extends LegacySingleWriteablePacket<KeepAlive> {
+
+	private static final InternalLogger logger = InternalLoggerFactory.getInstance(KeepAlivePacket.class);
+	private static final boolean debugConnection = Boolean.getBoolean("ps.debug.connection");
 
 	public KeepAlivePacket() {
 		super(LegacyPacketId.Dualbound.PLAY_KEEP_ALIVE);
@@ -13,6 +18,9 @@ public class KeepAlivePacket extends LegacySingleWriteablePacket<KeepAlive> {
 
 	@Override
 	protected void write(ByteBuf data, KeepAlive packet) {
+		if (debugConnection) {
+			logger.info("[ps-debug] native->legacy keepalive id={}", packet.getRandomId());
+		}
 		data.writeInt((int) packet.getRandomId());
 	}
 
--- src/main/java/protocolsupport/protocol/packet/middleimpl/writeable/play/v_4_5_6/PluginMessagePacket.java
+++ src/main/java/protocolsupport/protocol/packet/middleimpl/writeable/play/v_4_5_6/PluginMessagePacket.java
@@ -1,6 +1,8 @@
 package protocolsupport.protocol.packet.middleimpl.writeable.play.v_4_5_6;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.util.internal.logging.InternalLogger;
+import io.netty.util.internal.logging.InternalLoggerFactory;
 import net.md_5.bungee.protocol.packet.PluginMessage;
 import protocolsupport.protocol.packet.id.LegacyPacketId;
 import protocolsupport.protocol.packet.middleimpl.writeable.LegacySingleWriteablePacket;
@@ -8,6 +10,9 @@ import protocolsupport.protocol.serializer.ArraySerializer;
 import protocolsupport.protocol.serializer.StringSerializer;
 
 public class PluginMessagePacket extends LegacySingleWriteablePacket<PluginMessage> {
+
+	private static final InternalLogger logger = InternalLoggerFactory.getInstance(PluginMessagePacket.class);
+	private static final boolean debugConnection = Boolean.getBoolean("ps.debug.connection");
 
 	public PluginMessagePacket() {
 		super(LegacyPacketId.Dualbound.PLAY_PLUGIN_MESSAGE);
@@ -18,6 +23,9 @@ public class PluginMessagePacket extends LegacySingleWriteablePacket<PluginMessa
 		String tag = packet.getTag();
 		if (tag.equals("bungeecord:main")) {
 			tag = "BungeeCord";
+		}
+		if (debugConnection) {
+			logger.info("[ps-debug] native->legacy plugin message tag={} bytes={}", tag, packet.getData().length);
 		}
 		StringSerializer.writeShortUTF16BEString(data, tag);
 		ArraySerializer.writeShortLengthByteArray(data, packet.getData());
