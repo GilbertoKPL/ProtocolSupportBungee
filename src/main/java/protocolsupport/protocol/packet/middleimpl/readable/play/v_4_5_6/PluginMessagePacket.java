62ec84872a25d9fe833758f6bb37d97da9d6f21a
Gilberto <73399601+GilbertoKPL@users.noreply.github.com>
2026-04-19 10:26:36 -0300

    Log proxy-to-server handshake/login details in debug mode

--- src/main/java/protocolsupport/protocol/packet/middleimpl/readable/play/v_4_5_6/PluginMessagePacket.java
+++ src/main/java/protocolsupport/protocol/packet/middleimpl/readable/play/v_4_5_6/PluginMessagePacket.java
@@ -5,6 +5,8 @@ import java.util.Collections;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
+import io.netty.util.internal.logging.InternalLogger;
+import io.netty.util.internal.logging.InternalLoggerFactory;
 import net.md_5.bungee.protocol.PacketWrapper;
 import net.md_5.bungee.protocol.packet.PluginMessage;
 import protocolsupport.protocol.packet.id.LegacyPacketId;
@@ -13,6 +15,9 @@ import protocolsupport.protocol.serializer.ArraySerializer;
 import protocolsupport.protocol.serializer.StringSerializer;
 
 public class PluginMessagePacket extends LegacyDefinedReadableMiddlePacket {
+
+	private static final InternalLogger logger = InternalLoggerFactory.getInstance(PluginMessagePacket.class);
+	private static final boolean debugConnection = Boolean.getBoolean("ps.debug.connection");
 
 	public PluginMessagePacket() {
 		super(LegacyPacketId.Dualbound.PLAY_PLUGIN_MESSAGE);
@@ -25,6 +30,9 @@ public class PluginMessagePacket extends LegacyDefinedReadableMiddlePacket {
 	protected void read0(ByteBuf from) {
 		tag = StringSerializer.readShortUTF16BEString(from);
 		data = ArraySerializer.readShortLengthByteArray(from);
+		if (debugConnection) {
+			logger.info("[ps-debug] legacy->native plugin message tag={} bytes={}", tag, data.length);
+		}
 	}
 
 	@Override
--- src/main/java/protocolsupport/protocol/pipeline/common/LogicHandler.java
+++ src/main/java/protocolsupport/protocol/pipeline/common/LogicHandler.java
@@ -3,13 +3,20 @@ package protocolsupport.protocol.pipeline.common;
 import io.netty.channel.ChannelDuplexHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelPromise;
+import io.netty.util.internal.logging.InternalLogger;
+import io.netty.util.internal.logging.InternalLoggerFactory;
 import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.protocol.packet.Handshake;
+import net.md_5.bungee.protocol.packet.LoginRequest;
 import protocolsupport.api.events.ConnectionCloseEvent;
 import protocolsupport.api.events.ConnectionOpenEvent;
 import protocolsupport.protocol.ConnectionImpl;
 import protocolsupport.protocol.storage.ProtocolStorage;
 
 public class LogicHandler extends ChannelDuplexHandler {
+
+	private static final InternalLogger logger = InternalLoggerFactory.getInstance(LogicHandler.class);
+	private static final boolean debugConnection = Boolean.getBoolean("ps.debug.connection");
 
 	protected final ConnectionImpl connection;
 	protected final boolean isClientConnection;
@@ -34,6 +41,15 @@ public class LogicHandler extends ChannelDuplexHandler {
 			promise.setSuccess();
 			return;
 		}
+		if (debugConnection && !isClientConnection) {
+			if (msg instanceof Handshake) {
+				Handshake handshake = (Handshake) msg;
+				logger.info("[ps-debug] proxy->server handshake protocolVersion={} host={} port={} requestedProtocol={}",
+					handshake.getProtocolVersion(), handshake.getHost(), handshake.getPort(), handshake.getRequestedProtocol());
+			} else if (msg instanceof LoginRequest) {
+				logger.info("[ps-debug] proxy->server login request username={}", ((LoginRequest) msg).getData());
+			}
+		}
 		super.write(ctx, msg, promise);
 	}
 
@@ -41,7 +57,12 @@ public class LogicHandler extends ChannelDuplexHandler {
 	public void channelActive(ChannelHandlerContext ctx) throws Exception {
 		super.channelActive(ctx);
 		if (isClientConnection) {
+			if (debugConnection) {
+				logger.info("[ps-debug] client channel active address={} version={} profile={}", connection.getRawAddress(), connection.getVersion(), connection.getProfile());
+			}
 			ProxyServer.getInstance().getPluginManager().callEvent(new ConnectionOpenEvent(connection));
+		} else if (debugConnection) {
+			logger.info("[ps-debug] server channel active serverAddress={} clientAddress={} version={}", ctx.channel().remoteAddress(), connection.getRawAddress(), connection.getVersion());
 		}
 	}
 
@@ -49,8 +70,13 @@ public class LogicHandler extends ChannelDuplexHandler {
 	public void channelInactive(ChannelHandlerContext ctx) throws Exception {
 		super.channelInactive(ctx);
 		if (isClientConnection) {
+			if (debugConnection) {
+				logger.info("[ps-debug] client channel inactive address={} version={} player={} connected={}", connection.getRawAddress(), connection.getVersion(), connection.getPlayer(), connection.isConnected());
+			}
 			ProxyServer.getInstance().getPluginManager().callEvent(new ConnectionCloseEvent(connection));
 			ProtocolStorage.removeConnection(connection.getRawAddress());
+		} else if (debugConnection) {
+			logger.info("[ps-debug] server channel inactive serverAddress={} clientAddress={} version={} player={}", ctx.channel().remoteAddress(), connection.getRawAddress(), connection.getVersion(), connection.getPlayer());
 		}
 	}
 
